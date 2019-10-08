34820d9599219253c33e05bd60466e0a151be7ca
edgardoruiz <00170019@uca.edu.sv>
2019-10-07 20:38:42 -0600

    Actividades de clase

---
+++ resta.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b,c, resta;
+	
+	cout<<"Ingrese 3 numeros"<<endl;
+	cin>>a>>b>>c;
+	resta=a-b-c;
+	
+	cout<<"El resultado es:"<<resta<<endl;
+	
+	return 0;
+}
---
+++ suma.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a, b,c, suma=0;
+	cout<<"Ingrese 3 numeros"<<endl;
+	cin>>a>>b>>c;
+	
+	suma=a+b+c;
+	
+	cout<<"El resultado es :"<<suma<<endl;
+	
+	
+	
+	return 0;
+}
